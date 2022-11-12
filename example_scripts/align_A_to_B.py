9033c1280d1dd7de27c4be812af9822229b87da5
Simon Casassus <simoncasassus@gmail.com>
2022-11-12 16:06:58 +0000

    minor edit

--- example_scripts/align_A_to_B.py
+++ example_scripts/align_A_to_B.py
@@ -14,7 +14,7 @@ file_ms_ref = '/home/simon/HD135344B/data/calibrated_cont_self_LB19.ms'
 
 os.system("rm -rf "+file_ms_output)
 
-bestfit = np.load('output_align_A_to_B//bestfit_xcorr_wshift.npy')
+bestfit = np.load('output_align_A_to_B/bestfit_xcorr_wshift.npy')
 alpha_R = bestfit[0]
 delta_x = bestfit[1]
 delta_y = bestfit[2]
@@ -24,7 +24,5 @@ delta_y = bestfit[2]
 SBs = Dask_editms.apply_gain_shift(file_ms,
                                    file_ms_output=file_ms_output,
                                    alpha_R=alpha_R,
-                                   datacolumn='DATA',
-                                   datacolumns_output='DATA',
                                    Shift=[delta_x, delta_y],
                                    file_ms_ref=file_ms_ref)
